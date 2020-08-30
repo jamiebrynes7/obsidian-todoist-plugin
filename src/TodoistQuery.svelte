6c0d74e39cd6127d91ec21b2b6b853c4d5535db7
Jamie Brynes <jamiebrynes7@gmail.com>
2020-08-30 19:29:58 +0100

    autorefresh functionality

--- src/TodoistQuery.svelte
+++ src/TodoistQuery.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { onMount } from "svelte";
+  import { onMount, onDestroy } from "svelte";
   import { fade } from "svelte/transition";
 
   export let query;
@@ -10,7 +10,20 @@
   let tasksPendingClose = [];
   $: todos = tasks.filter(task => !tasksPendingClose.includes(task.id));
 
-  onMount(async () => { await fetchTodos();});
+  let autoRefreshIntervalId = null;
+
+  onMount(async () => { 
+    await fetchTodos();
+    if (query?.autorefresh) {
+      autoRefreshIntervalId = setInterval(async () => { await fetchTodos(); }, query.autorefresh * 1000);
+    }
+  });
+
+  onDestroy(() => {
+    if (autoRefreshIntervalId != null) {
+      clearInterval(autoRefreshIntervalId);
+    }
+  });
 
   async function onClickTask(task) {
     tasksPendingClose.push(task.id);
@@ -64,7 +77,7 @@
   }
 </script>
 
-<h4 class="todoist-query-title">{ query.name }</h4> 
+<h4 class="todoist-query-title">{ query.name }</h4>
 <button
   class="todoist-refresh-button"
   on:click={async () => { await fetchTodos() }}
@@ -73,9 +86,9 @@
   <svg
     class="{fetching ? 'todoist-refresh-spin' : ''}"
     width="20px" height="20px"
-    viewBox="0 0 20 20" 
+    viewBox="0 0 20 20"
     fill="currentColor"
-    xmlns="http://www.w3.org/2000/svg" 
+    xmlns="http://www.w3.org/2000/svg"
     >
       <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
   </svg>
