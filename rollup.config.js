359c8e2e2eb5b013ea5c630ea2886544e78b73df
Jamie Brynes <jamiebrynes7@gmail.com>
2020-08-31 12:58:38 +0100

    setup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import autoPreprocess from 'svelte-preprocess';
 
 export default {
   input: 'src/index.js',
@@ -10,7 +12,10 @@ export default {
     file: 'dist/todoist.js'
   },
   plugins: [
-    svelte(),
+    svelte({
+      preprocess: autoPreprocess()
+    }),
+    typescript(),
     resolve({
       browser: true,
       dedupe: ['svelte']
