f0cb2d52d3af6eba292815449235e1166e9d4563
Jamie Brynes <jamiebrynes7@gmail.com>
2020-09-02 21:14:03 +0100

    build task tree

--- src/api.ts
+++ src/api.ts
@@ -5,7 +5,7 @@ export class TodoistApi {
     this.token = token;
   }
 
-  async getTasks(filter?: string): Promise<ITask[]> {
+  async getTasks(filter?: string): Promise<Task[]> {
     let url = "https://api.todoist.com/rest/v1/tasks";
 
     if (filter) {
@@ -18,7 +18,8 @@ export class TodoistApi {
       }),
     });
 
-    return (await result.json()) as ITask[];
+    const tasks = (await result.json()) as IApiTask[];
+    return Task.buildTree(tasks);
   }
 
   async closeTask(id: ID): Promise<boolean> {
@@ -35,11 +36,48 @@ export class TodoistApi {
   }
 }
 
-export type ID = string;
+export type ID = number;
 
-export interface ITask {
+interface IApiTask {
   id: ID;
   priority: number;
   content: string;
   order: number;
+  parent?: ID;
+}
+
+export class Task {
+  public id: ID;
+  public priority: number;
+  public content: string;
+  public order: number;
+  public parent?: Task;
+  public children: Task[];
+
+  constructor(raw: IApiTask) {
+    this.id = raw.id;
+    this.priority = raw.priority;
+    this.content = raw.content;
+    this.order = raw.order;
+    this.children = [];
+  }
+
+  static buildTree(tasks: IApiTask[]): Task[] {
+    const mapping = new Map<ID, Task>();
+
+    tasks.forEach(task => mapping.set(task.id, new Task(task)));
+    tasks.forEach(task => {
+      if (task.parent == null || !mapping.has(task.parent)) {
+        return;
+      }
+
+      const self = mapping.get(task.id);
+      const parent = mapping.get(task.parent);
+
+      self.parent = parent;
+      parent.children.push(self);
+    });
+
+    return Array.from(mapping.values()).filter(task => task.parent == null);
+  }
 }
