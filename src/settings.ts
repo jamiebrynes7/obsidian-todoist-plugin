95134a5bac10fcf65f530a9cb9295ac168ff6598
Jamie Brynes <jamiebrynes7@gmail.com>
2020-09-22 19:56:02 +0100

    add changelog link

--- src/settings.ts
+++ src/settings.ts
@@ -59,11 +59,7 @@ export function SettingsTab<TBase extends Settings>(Base: TBase) {
       this.settingMgr = new SettingManager(this.containerEl);
       this.settingMgr.empty();
 
-      this.settingMgr.addStaticText({
-        name: `Current Version: ${this.plugin.version}`,
-        description: "",
-        configure: () => {},
-      });
+      this.pluginMetadata();
 
       this.fadeAnimationSettings();
       this.autoRefreshSettings();
@@ -71,6 +67,28 @@ export function SettingsTab<TBase extends Settings>(Base: TBase) {
       this.dateSettings();
       this.projectSettings();
       this.labelsSettings();
+    }
+
+    pluginMetadata() {
+      const desc = document.createElement("div") as HTMLDivElement;
+
+      const span = document.createElement("span") as HTMLSpanElement;
+      span.innerText = "Read the ";
+
+      const changelogLink = document.createElement("a") as HTMLAnchorElement;
+      changelogLink.href =
+        "https://github.com/jamiebrynes7/obsidian-todoist-plugin/releases";
+      changelogLink.innerText = "changelog!";
+
+      span.appendChild(changelogLink);
+
+      desc.appendChild(span);
+
+      this.settingMgr.addStaticText({
+        name: `Current Version: ${this.plugin.version}`,
+        description: desc,
+        configure: () => {},
+      });
     }
 
     fadeAnimationSettings() {
@@ -244,7 +262,13 @@ class SettingManager {
 
     const desc = document.createElement("div") as HTMLDivElement;
     desc.classList.add("setting-item-description");
-    desc.innerText = config.description;
+    if (typeof config.description == "string") {
+      const description = config.description as string;
+      desc.innerText = description;
+    } else {
+      const descriptionBlock = config.description as HTMLDivElement;
+      desc.appendChild(descriptionBlock);
+    }
     info.appendChild(desc);
 
     const control = document.createElement("div") as HTMLDivElement;
@@ -257,7 +281,7 @@ class SettingManager {
 
 interface ISettingConfiguration<TSettings> {
   name: string;
-  description: string;
+  description: string | HTMLDivElement;
   configure: (settings: TSettings) => void;
 }
 
