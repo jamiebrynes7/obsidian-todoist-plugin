110dff9eebb94fbd07db6eb425c5fa42b01dc761
Jamie Brynes <jamiebrynes7@gmail.com>
2020-12-31 18:02:04 +0000

    use svelte for token modal

---
+++ src/modals/enterToken/enterTokenModal.ts
@@ -0,0 +1,40 @@
+import { App, Modal } from "obsidian";
+import EnterTokenModalContent from "./EnterTokenModalContent.svelte";
+
+export default class TodoistApiTokenModal extends Modal {
+  public token: string;
+  public waitForClose: Promise<void>;
+
+  private resolvePromise: () => void;
+
+  private modalContent: EnterTokenModalContent;
+
+  constructor(app: App) {
+    super(app);
+
+    this.token = "";
+    this.waitForClose = new Promise(
+      (resolve) => (this.resolvePromise = resolve)
+    );
+
+    this.titleEl.innerText = "Setup Todoist API token";
+
+    this.modalContent = new EnterTokenModalContent({
+      target: this.contentEl,
+      props: {
+        onSubmit: (value: string) => {
+          this.token = value;
+          this.close();
+        },
+      },
+    });
+
+    this.open();
+  }
+
+  onClose() {
+    super.onClose();
+    this.modalContent.$destroy();
+    this.resolvePromise();
+  }
+}
