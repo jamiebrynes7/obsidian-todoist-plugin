jamiebrynes7/obsidian-todoist-plugin

commit fc61992ee786aa71daa1508f5170097eabbf6c8d

Add command for inserting Obsidian URI into task (#84)

src/index.ts
@@ -3,7 +3,7 @@ import { TodoistApi } from "./api/api";
 import debug from "./log";
 import { App, Plugin, PluginManifest } from "obsidian";
 import TodoistApiTokenModal from "./modals/enterToken/enterTokenModal";
-import { getTokenPath } from "./utils";
+import { getCurrentPageMdLink, getTokenPath } from "./utils";
 import CreateTaskModal from "./modals/createTask/createTaskModal";
 import QueryInjector from "./queryInjector";
 
@@ -61,6 +61,20 @@ export default class TodoistPlugin extends Plugin {
           this.app,
           this.api,
           window.getSelection().toString()
+        );
+      },
+    });
+
+    this.addCommand({
+      id: "todoist-add-task",
+      name: "Add Todoist task with the current page",
+      callback: () => {
+        const currentSelection = window.getSelection().toString();
+        new CreateTaskModal(
+          this.app,
+          this.api,
+          `${currentSelection} ${getCurrentPageMdLink(this.app)}`,
+          currentSelection.length
         );
       },
     });

src/modals/createTask/CreateTaskModalContent.svelte
@@ -15,6 +15,7 @@
   export let api: TodoistApi;
   export let close: () => void;
   export let value: string;
+  export let initialCursorPosition: number | undefined;
 
   let activeLabels: LabelOption[] = null;
   let activeProject: ProjectOption = null;
@@ -28,6 +29,11 @@
   onMount(async () => {
     await tick();
     inputEl.focus();
+
+    if (typeof initialCursorPosition != "undefined") {
+      inputEl.setSelectionRange(initialCursorPosition, initialCursorPosition);
+      inputEl.scrollLeft = 0;
+    }
   });
 
   async function triggerClose() {

src/modals/createTask/createTaskModal.ts
@@ -5,7 +5,12 @@ import CreateTaskModalContent from "./CreateTaskModalContent.svelte";
 export default class CreateTaskModal extends Modal {
   private readonly modalContent: CreateTaskModalContent;
 
-  constructor(app: App, api: TodoistApi, initialValue?: string) {
+  constructor(
+    app: App,
+    api: TodoistApi,
+    initialValue: string,
+    initialCursorPosition?: number
+  ) {
     super(app);
 
     this.titleEl.innerText = "Create new Todoist task";
@@ -16,6 +21,7 @@ export default class CreateTaskModal extends Modal {
         api: api,
         close: () => this.close(),
         value: initialValue,
+        initialCursorPosition: initialCursorPosition,
       },
     });
 

src/modals/enterToken/EnterTokenModalContent.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  export let onSubmit: (string) => void;
+  export let onSubmit: (value: string) => void;
 
   let tokenInput: HTMLInputElement;
 </script>

src/utils.ts
@@ -1,3 +1,5 @@
+import type { App } from "obsidian";
+
 const proc = require("process");
 
 export function isPositiveInteger(str: string): boolean {
@@ -54,4 +56,20 @@ export function getTokenPath(): string {
   }
 
   return `.obsidian${pathSep}todoist-token`;
+}
+
+export function getCurrentPageMdLink(app: App): string {
+  const vaultName = app.vault.adapter.getName();
+
+  const currentView = app.workspace.activeLeaf.view;
+
+  if (currentView.getViewType() != "markdown") {
+    return "";
+  }
+
+  const filePath: string = app.workspace.activeLeaf.view.getState().file;
+
+  return `[${filePath}](obsidian://open?vault=${encodeURIComponent(
+    vaultName
+  )}&file=${encodeURIComponent(filePath)})`;
 }

commit 333e516fe311b10793f55f43dbb969d6a694606a

fix issue introduced in #84

src/index.ts
@@ -66,7 +66,7 @@ export default class TodoistPlugin extends Plugin {
     });
 
     this.addCommand({
-      id: "todoist-add-task",
+      id: "todoist-add-task-current-page",
       name: "Add Todoist task with the current page",
       callback: () => {
         const currentSelection = window.getSelection().toString();