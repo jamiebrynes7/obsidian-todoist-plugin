52dfbf8bf58bc13dc7d549d1607e7507239a3c70
Jamie Brynes <jamiebrynes7@gmail.com>
2022-08-10 19:38:45 +0100

    support custom obsidian directory for token (#168)

--- src/index.ts
+++ src/index.ts
@@ -80,7 +80,7 @@ export default class TodoistPlugin extends Plugin {
       },
     });
 
-    const tokenPath = getTokenPath();
+    const tokenPath = getTokenPath(app.vault);
     try {
       const token = await this.app.vault.adapter.read(tokenPath);
       this.api = new TodoistApi(token);
