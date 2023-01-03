c8653b46c31e637a8c92223a870b282b1b154011
Jamie Brynes <jamiebrynes7@gmail.com>
2023-01-03 13:45:17 -0800

    refactor out common markdown renderer

--- src/ui/DescriptionRenderer.svelte
+++ src/ui/DescriptionRenderer.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-  import { MarkdownRenderer } from "obsidian";
-  import { afterUpdate, onMount } from "svelte";
+  import MarkdownRenderer from "../components/MarkdownRenderer.svelte";
 
   export let description: string;
-
-  let containerEl: HTMLDivElement;
-  let isExpandedContent: boolean = false;
 
   $: isComplex =
     description.contains("\n") ||
@@ -14,43 +10,21 @@
     description.startsWith("-") ||
     description.startsWith("1.");
 
-  onMount(async () => {
-    if (containerEl === undefined) {
-      return;
-    }
-
-    await MarkdownRenderer.renderMarkdown(description, containerEl, "", null);
-
-    if (containerEl.childElementCount > 1) {
-      return;
-    }
-
-    const markdownContent = containerEl.querySelector("p");
-
-    if (markdownContent) {
-      markdownContent.parentElement.removeChild(markdownContent);
-      containerEl.innerHTML = markdownContent.innerHTML;
-    }
-  });
+  $: isExpandedContent = !isComplex;
 
   function toggleExpandedContent() {
+    if (!isComplex) {
+      return;
+    }
+
     isExpandedContent = !isExpandedContent;
   }
 </script>
 
-{#if isComplex}
-  <div class="todoist-task-description" on:dblclick={toggleExpandedContent}>
-    <div bind:this={containerEl} class={isExpandedContent ? "" : "hidden"} />
-    <span class={isExpandedContent ? "hidden" : ""}
-      >{description.split("\n")[0]}...</span
-    >
-  </div>
-{:else}
-  <div bind:this={containerEl} class="todoist-task-description" />
-{/if}
-
-<style>
-  .hidden {
-    display: none;
-  }
-</style>
+<div class="todoist-task-description" on:dblclick={toggleExpandedContent}>
+  {#if isExpandedContent}
+    <MarkdownRenderer content={description} />
+  {:else}
+    <span>{description.split("\n")[0]}...</span>
+  {/if}
+</div>
