71bc89d7d572600409194a60fa2b370f6fbbfb1c
Jamie Brynes <jamiebrynes7@gmail.com>
2023-11-26 00:46:23 +0000

    Migrate tests into src/ dir (#260)

---
+++ src/api/task.test.ts
@@ -0,0 +1,359 @@
+import "mocha";
+import { assert } from "chai";
+import { Task } from "./models";
+import type { ITaskRaw } from "./raw_models";
+
+describe("Task tree parsing", () => {
+    it("Tree matches parents and subtasks", () => {
+        const apiTasks: ITaskRaw[] = [
+            {
+                id: "1",
+                priority: 1,
+                order: 0,
+                content: "Parent task",
+                description: "",
+                project_id: "123",
+                section_id: null,
+                labels: [],
+            },
+            {
+                id: "2",
+                priority: 2,
+                order: 1,
+                content: "Subtask",
+                description: "",
+                parent_id: "1",
+                project_id: "123",
+                section_id: null,
+                labels: [],
+            },
+        ];
+
+        const tasks = Task.buildTree(apiTasks);
+        assert.lengthOf(tasks, 1, "Resulting tree has one parent node.");
+
+        const parent = tasks[0];
+        assert.lengthOf(parent.children, 1, "Parent task has one subtask");
+
+        const child = parent.children[0];
+        assert.equal(child.id, "2", "Parent has expected subtask");
+        assert.equal(child.parent, parent, "Subtask has expected parent.");
+    });
+
+    it("Subtask without parent in scope is a top-level node", () => {
+        const apiTasks: ITaskRaw[] = [
+            {
+                id: "2",
+                priority: 2,
+                order: 1,
+                content: "Subtask",
+                description: "",
+                parent_id: "1",
+                project_id: "123",
+                section_id: null,
+                labels: [],
+            },
+        ];
+
+        const tasks = Task.buildTree(apiTasks);
+        assert.lengthOf(tasks, 1, "Resulting tree has one parent node.");
+    });
+});
+
+describe("Task date parsing", () => {
+    it("Tasks with dates only have no time", () => {
+        const apiTask: ITaskRaw = {
+            id: "1",
+            priority: 1,
+            order: 1,
+            content: "Task",
+            description: "",
+            due: {
+                date: "2019-09-12",
+                datetime: null,
+                recurring: false,
+            },
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        };
+
+        const task = new Task(apiTask);
+
+        assert.isFalse(task.hasTime);
+    });
+
+    it("Tasks with time have time", () => {
+        const apiTask: ITaskRaw = {
+            id: "1",
+            priority: 1,
+            order: 1,
+            content: "Task",
+            description: "",
+            due: {
+                date: null,
+                datetime: "2019-09-12T02:30:00Z",
+                recurring: false,
+            },
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        };
+
+        const task = new Task(apiTask);
+
+        assert.isTrue(task.hasTime);
+    });
+});
+
+describe("Task comparisons", () => {
+    it("Tasks use Todoist order if no ordering provided", () => {
+        const first = new Task({
+            id: "1",
+            priority: 1,
+            order: 0,
+            content: "Parent task",
+            description: "",
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        });
+
+        const second = new Task({
+            id: "2",
+            priority: 2,
+            order: 1,
+            content: "Subtask",
+            description: "",
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        });
+
+        assert.isBelow(first.compareTo(second, []), 0);
+        assert.isAbove(second.compareTo(first, []), 0);
+    });
+
+    it("Tasks are sorted by priority (high to low)", () => {
+        const lowPriority = new Task({
+            id: "1",
+            priority: 1,
+            order: 0,
+            content: "Parent task",
+            description: "",
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        });
+
+        const highPriority = new Task({
+            id: "2",
+            priority: 2,
+            order: 1,
+            content: "Subtask",
+            description: "",
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        });
+
+        assert.isBelow(highPriority.compareTo(lowPriority, ["priority"]), 0);
+        assert.isAbove(lowPriority.compareTo(highPriority, ["priority"]), 0);
+    });
+
+    it("Tasks fallback to Todoist ordering if priority is the same", () => {
+        const first = new Task({
+            id: "1",
+            priority: 2,
+            order: 0,
+            content: "Parent task",
+            description: "",
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        });
+
+        const second = new Task({
+            id: "2",
+            priority: 2,
+            order: 1,
+            content: "Subtask",
+            description: "",
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        });
+
+        assert.isBelow(first.compareTo(second, []), 0);
+        assert.isAbove(second.compareTo(first, []), 0);
+    });
+
+    it("Tasks are sorted by date (earliest -> latest).", () => {
+        const first = new Task({
+            id: "1",
+            priority: 2,
+            order: 0,
+            content: "Parent task",
+            description: "",
+            due: {
+                recurring: false,
+                date: "2019-09-01",
+                datetime: null,
+            },
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        });
+
+        const second = new Task({
+            id: "2",
+            priority: 2,
+            order: 1,
+            content: "Subtask",
+            description: "",
+            due: {
+                recurring: false,
+                date: "2019-09-07",
+                datetime: null,
+            },
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        });
+
+        assert.isBelow(first.compareTo(second, ["date"]), 0);
+        assert.isAbove(second.compareTo(first, ["date"]), 0);
+    });
+
+    it("Tasks on the same day are sorted in time order (earliest -> latest -> no time)", () => {
+        const first = new Task({
+            id: "1",
+            priority: 2,
+            order: 0,
+            content: "Parent task",
+            description: "",
+            due: {
+                recurring: false,
+                date: null,
+                datetime: "2019-08-01T02:30:00Z",
+            },
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        });
+
+        const second = new Task({
+            id: "2",
+            priority: 2,
+            order: 1,
+            content: "Subtask",
+            description: "",
+            due: {
+                recurring: false,
+                date: null,
+                datetime: "2019-08-01T03:30:00Z",
+            },
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        });
+
+        const third = new Task({
+            id: "2",
+            priority: 2,
+            order: 1,
+            content: "Subtask",
+            description: "",
+            due: {
+                recurring: false,
+                date: "2019-08-01",
+                datetime: null,
+            },
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        });
+
+        assert.isBelow(first.compareTo(second, ["date"]), 0);
+        assert.isBelow(first.compareTo(third, ["date"]), 0);
+        assert.isBelow(second.compareTo(third, ["date"]), 0);
+
+        assert.isAbove(second.compareTo(first, ["date"]), 0);
+        assert.isAbove(third.compareTo(first, ["date"]), 0);
+        assert.isAbove(third.compareTo(second, ["date"]), 0);
+    });
+
+    it("Task ordered by priority falls to the next if equal", () => {
+        const first = new Task({
+            id: "1",
+            priority: 1,
+            order: 1,
+            content: "Task",
+            description: "",
+            due: {
+                recurring: false,
+                date: "2019-08-02",
+                datetime: null,
+            },
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        });
+
+        const second = new Task({
+            id: "1",
+            priority: 1,
+            order: 1,
+            content: "Task",
+            description: "",
+            due: {
+                recurring: false,
+                date: "2019-08-03",
+                datetime: null,
+            },
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        });
+
+        assert.isBelow(first.compareTo(second, ["priority", "date"]), 0);
+        assert.isAbove(second.compareTo(first, ["priority", "date"]), 0);
+    });
+
+    it("Task ordered by date falls to the next if equal", () => {
+        const first = new Task({
+            id: "1",
+            priority: 2,
+            order: 1,
+            content: "Task",
+            description: "",
+            due: {
+                recurring: false,
+                date: "2019-08-03",
+                datetime: null,
+            },
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        });
+
+        const second = new Task({
+            id: "1",
+            priority: 1,
+            order: 1,
+            content: "Task",
+            description: "",
+            due: {
+                recurring: false,
+                date: "2019-08-03",
+                datetime: null,
+            },
+            project_id: "123",
+            section_id: null,
+            labels: [],
+        });
+
+        assert.isBelow(first.compareTo(second, ["date", "priority"]), 0);
+        assert.isAbove(second.compareTo(first, ["date", "priority"]), 0);
+    });
+});
---
+++ src/query/parser.test.ts
@@ -0,0 +1,114 @@
+import "mocha";
+import { assert } from "chai";
+import { parseQuery, ParsingError } from "./parser";
+
+describe("parseQuery", () => {
+    it("Name must exist", () => {
+        const obj = {
+            filter: "foo",
+        };
+
+        assert.throws(() => { parseQuery(JSON.stringify(obj)); }, ParsingError);
+    });
+
+    it("Filter must exist", () => {
+        const obj = {
+            name: "Tasks",
+        };
+
+        assert.throws(() => { parseQuery(JSON.stringify(obj)); }, ParsingError);
+    });
+
+    it("Only name & filter are required", () => {
+        const obj = {
+            name: "Tasks",
+            filter: "foo",
+        };
+
+        assert.doesNotThrow(() => parseQuery(JSON.stringify(obj)), ParsingError);
+    });
+
+    it("Autorefresh must be a number", () => {
+        const obj = {
+            name: "Tasks",
+            filter: "foo",
+            autorefresh: "NaN",
+        };
+
+        assert.throws(() => { parseQuery(JSON.stringify(obj)); }, ParsingError);
+    });
+
+    it("Autorefresh cannot be negative", () => {
+        const obj = {
+            name: "Tasks",
+            filter: "foo",
+            autorefresh: -1,
+        };
+
+        assert.throws(() => { parseQuery(JSON.stringify(obj)); }, ParsingError);
+    });
+
+    it("Valid autorefresh values pass", () => {
+        const obj = {
+            name: "Tasks",
+            filter: "foo",
+            autorefresh: 1,
+        };
+
+        assert.doesNotThrow(() => parseQuery(JSON.stringify(obj)), ParsingError);
+    });
+
+    it("Sorting must be an array", () => {
+        const obj = {
+            name: "Tasks",
+            filter: "foo",
+            sorting: "Not an array",
+        };
+
+        assert.throws(() => { parseQuery(JSON.stringify(obj)); }, ParsingError);
+
+        const other = {
+            name: "Tasks",
+            filter: "foo",
+            sorting: [],
+        };
+
+        assert.doesNotThrow(() => { parseQuery(JSON.stringify(other)); }, ParsingError);
+    });
+
+    it("Sorting can only be a specified set of options", () => {
+        const obj = {
+            name: "Tasks",
+            filter: "filter",
+            sorting: ["not-valid"],
+        };
+
+        assert.throws(() => { parseQuery(JSON.stringify(obj)); }, ParsingError);
+
+        const validObj = {
+            name: "Tasks",
+            filter: "filter",
+            sorting: ["date", "priority"],
+        };
+
+        assert.doesNotThrow(() => parseQuery(JSON.stringify(validObj)), ParsingError);
+    });
+
+    it("Group must be a boolean", () => {
+        const obj = {
+            name: "Tasks",
+            filter: "Filter",
+            group: "not-a-boolean",
+        };
+
+        assert.throws(() => { parseQuery(JSON.stringify(obj)); }, ParsingError);
+
+        const validObj = {
+            name: "Tasks",
+            filter: "Filter",
+            group: true,
+        };
+
+        assert.doesNotThrow(() => parseQuery(JSON.stringify(validObj)), ParsingError);
+    });
+});
