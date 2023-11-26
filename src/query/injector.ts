cdd6e9fff4e5df459a683d9e92d4fe5a5529e29f
Jamie Brynes <jamiebrynes7@gmail.com>
2023-11-26 00:36:28 +0000

    Add filename replacement functionality (#259)
    Co-authored-by: anth-dinosaur <116119475+anth-dinosaur@users.noreply.github.com>

--- src/query/injector.ts
+++ src/query/injector.ts
@@ -6,99 +6,101 @@ import { parseQuery } from "./parser";
 import TodoistQuery from "../ui/TodoistQuery.svelte";
 import ErrorDisplay from "../ui/ErrorDisplay.svelte";
 import type { SvelteComponentDev } from "svelte/internal";
+import { applyReplacements } from "./replacements";
 
 export class QueryInjector {
-  private api: TodoistApi;
-  private app: App;
-  private pendingQueries: PendingQuery[];
+    private api: TodoistApi;
+    private app: App;
+    private pendingQueries: PendingQuery[];
 
-  constructor(app: App) {
-    this.app = app;
-    this.pendingQueries = [];
-  }
-
-  onNewBlock(
-    source: string,
-    el: HTMLElement,
-    ctx: MarkdownPostProcessorContext
-  ) {
-    const pendingQuery = {
-      source: source,
-      target: el,
-      ctx: ctx,
-    };
-
-    if (typeof this.api == "undefined") {
-      this.pendingQueries.push(pendingQuery);
-      return;
+    constructor(app: App) {
+        this.app = app;
+        this.pendingQueries = [];
     }
 
-    this.injectQuery(pendingQuery);
-  }
+    onNewBlock(
+        source: string,
+        el: HTMLElement,
+        ctx: MarkdownPostProcessorContext
+    ) {
+        const pendingQuery = {
+            source: source,
+            target: el,
+            ctx: ctx,
+        };
 
-  setApi(api: TodoistApi) {
-    this.api = api;
+        if (typeof this.api == "undefined") {
+            this.pendingQueries.push(pendingQuery);
+            return;
+        }
 
-    while (this.pendingQueries.length > 0) {
-      this.injectQuery(this.pendingQueries[0]);
-      this.pendingQueries.splice(0, 1);
-    }
-  }
-
-  injectQuery(pendingQuery: PendingQuery) {
-    let child: InjectedQuery;
-
-    try {
-      const query = parseQuery(pendingQuery.source);
-
-      debug({
-        msg: "Parsed query",
-        context: query,
-      });
-
-      child = new InjectedQuery(pendingQuery.target, (root: HTMLElement) => {
-        return new TodoistQuery({
-          target: root,
-          props: { query: query, api: this.api, app: this.app },
-        });
-      });
-    } catch (e) {
-      console.error(e);
-      child = new InjectedQuery(pendingQuery.target, (root: HTMLElement) => {
-        return new ErrorDisplay({ target: root, props: { error: e } });
-      });
+        this.injectQuery(pendingQuery);
     }
 
-    pendingQuery.ctx.addChild(child);
-  }
+    setApi(api: TodoistApi) {
+        this.api = api;
+
+        while (this.pendingQueries.length > 0) {
+            this.injectQuery(this.pendingQueries[0]);
+            this.pendingQueries.splice(0, 1);
+        }
+    }
+
+    injectQuery(pendingQuery: PendingQuery) {
+        let child: InjectedQuery;
+
+        try {
+            const query = parseQuery(pendingQuery.source);
+            applyReplacements(query, pendingQuery.ctx);
+
+            debug({
+                msg: "Parsed query",
+                context: query,
+            });
+
+            child = new InjectedQuery(pendingQuery.target, (root: HTMLElement) => {
+                return new TodoistQuery({
+                    target: root,
+                    props: { query: query, api: this.api, app: this.app },
+                });
+            });
+        } catch (e) {
+            console.error(e);
+            child = new InjectedQuery(pendingQuery.target, (root: HTMLElement) => {
+                return new ErrorDisplay({ target: root, props: { error: e } });
+            });
+        }
+
+        pendingQuery.ctx.addChild(child);
+    }
 }
 
 interface PendingQuery {
-  source: string;
-  target: HTMLElement;
-  ctx: MarkdownPostProcessorContext;
+    source: string;
+    target: HTMLElement;
+    ctx: MarkdownPostProcessorContext;
 }
 
 class InjectedQuery extends MarkdownRenderChild {
-  private readonly createComp: (root: HTMLElement) => SvelteComponentDev;
-  private component: SvelteComponentDev;
+    private readonly createComp: (root: HTMLElement) => SvelteComponentDev;
+    private component: SvelteComponentDev;
 
-  constructor(
-    container: HTMLElement,
-    createComp: (root: HTMLElement) => SvelteComponentDev
-  ) {
-    super(container);
-    this.createComp = createComp;
-    this.containerEl = container;
-  }
-
-  onload() {
-    this.component = this.createComp(this.containerEl);
-  }
-
-  onunload() {
-    if (this.component) {
-      this.component.$destroy();
+    constructor(
+        container: HTMLElement,
+        createComp: (root: HTMLElement) => SvelteComponentDev
+    ) {
+        super(container);
+        this.createComp = createComp;
+        this.containerEl = container;
     }
-  }
+
+    onload() {
+        this.component = this.createComp(this.containerEl);
+    }
+
+    onunload() {
+        if (this.component) {
+            this.component.$destroy();
+        }
+    }
 }
