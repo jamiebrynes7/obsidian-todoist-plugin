c1b906673ff1e0b69000edd42b5e0aa8433a059f
Jamie Brynes <jamiebrynes7@gmail.com>
2024-03-07 16:59:01 +0000

    feature: rebuild onboarding modal
    This commit does a few things:
    
    - it rebuilds the token entering modal completely
    - it improves the UX by not alert()'ing when there is no token
    - it will open the modal on each startup, if the token still doesn't exist

--- plugin/src/token.ts
+++ plugin/src/token.ts
@@ -1,5 +1,22 @@
 import type { Vault } from "obsidian";
 
-export function getTokenPath(vault: Vault): string {
-  return `${vault.configDir}/todoist-token`;
+export class VaultTokenAccessor {
+  private readonly vault: Vault;
+  private readonly path: string;
+  constructor(vault: Vault) {
+    this.vault = vault;
+    this.path = `${vault.configDir}/todoist-token`;
+  }
+
+  exists(): Promise<boolean> {
+    return this.vault.adapter.exists(this.path);
+  }
+
+  read(): Promise<string> {
+    return this.vault.adapter.read(this.path);
+  }
+
+  write(token: string): Promise<void> {
+    return this.vault.adapter.write(this.path, token);
+  }
 }
---
+++ plugin/src/ui/onboardingModal/index.tsx
@@ -0,0 +1,60 @@
+import { App, Modal, Notice } from "obsidian";
+import React from "react";
+import { type Root, createRoot } from "react-dom/client";
+import { TokenInputForm } from "./TokenInputForm";
+import "./styles.scss";
+
+type OnTokenSubmitted = (token: string) => Promise<void>;
+
+export class OnboardingModal extends Modal {
+  private readonly onTokenSubmitted: OnTokenSubmitted;
+  private readonly reactRoot: Root;
+
+  constructor(app: App, onTokenSubmitted: OnTokenSubmitted) {
+    super(app);
+    this.onTokenSubmitted = onTokenSubmitted;
+    this.titleEl.textContent = "Sync with Todoist Setup";
+
+    const { contentEl } = this;
+    contentEl.empty();
+    this.reactRoot = createRoot(contentEl);
+
+    const callback = (token: string) => {
+      this.close();
+      this.onTokenSubmitted(token).catch((e) => {
+        console.error("Failed to save API token", e);
+        new Notice("Failed to save API token");
+      });
+    };
+
+    this.reactRoot.render(<ModalRoot onTokenSubmit={callback} />);
+  }
+
+  onClose(): void {
+    super.onClose();
+    this.reactRoot.unmount();
+  }
+}
+
+type Props = {
+  onTokenSubmit: (token: string) => void;
+};
+
+const ModalRoot: React.FC<Props> = ({ onTokenSubmit }) => {
+  return (
+    <div className="onboarding-modal-root">
+      <p>
+        In order to use this plugin, you must provide your Todoist API token. This allows us to read
+        and write data to or from your Todoist account.
+      </p>
+      <p>
+        You can follow{" "}
+        <a href="https://todoist.com/help/articles/find-your-api-token-Jpzx9IIlB">
+          Todoist's guide
+        </a>{" "}
+        on finding your API token.
+      </p>
+      <TokenInputForm onTokenSubmit={onTokenSubmit} />
+    </div>
+  );
+};
