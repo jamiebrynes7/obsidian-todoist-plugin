09081383578f71c86ad9c253f6a438e0c74dd842
Jamie Brynes <jamiebrynes7@gmail.com>
2024-11-13 17:52:11 +0000

    fix: don't show empty query display if API not ready yet

---
+++ plugin/src/ui/query/displays/NotReadyDisplay.tsx
@@ -0,0 +1,3 @@
+export const NotReadyDisplay: React.FC = () => {
+  return <></>;
+};
--- plugin/src/ui/query/displays/index.ts
+++ plugin/src/ui/query/displays/index.ts
@@ -2,10 +2,12 @@ import { EmptyDisplay } from "@/ui/query/displays/EmptyDisplay";
 import { ErrorDisplay } from "@/ui/query/displays/ErrorDisplay";
 import { GroupedDisplay } from "@/ui/query/displays/GroupedDisplay";
 import { ListDisplay } from "@/ui/query/displays/ListDisplay";
+import { NotReadyDisplay } from "@/ui/query/displays/NotReadyDisplay";
 
 export const Displays = {
   Error: ErrorDisplay,
   Empty: EmptyDisplay,
   List: ListDisplay,
   Grouped: GroupedDisplay,
+  NotReady: NotReadyDisplay,
 };
