d8af5400d9a5a7a744b744179799ef4c9bb9c3db
Jamie Brynes <jamiebrynes7@gmail.com>
2024-12-28 19:10:29 +0000

    chore: add additional tests

--- plugin/src/data/dueDate.test.ts
+++ plugin/src/data/dueDate.test.ts
@@ -186,4 +186,309 @@ describe("isTomorrow", () => {
   }
 });
 
-// TODO: Write tests for other is* methods.
+describe("isYesterday", () => {
+  const testcases: TestCase[] = [
+    {
+      description: "should be true for day before",
+      input: {
+        isRecurring: false,
+        date: "2023-12-31",
+      },
+      expected: true,
+    },
+    {
+      description: "should be false for any other previous day",
+      input: {
+        isRecurring: false,
+        date: "2023-12-28",
+      },
+      expected: false,
+    },
+    {
+      description: "should be false for same day",
+      input: {
+        isRecurring: false,
+        date: "2024-01-01",
+      },
+      expected: false,
+    },
+    {
+      description: "should be false for any other future day",
+      input: {
+        isRecurring: false,
+        date: "2024-01-02",
+      },
+      expected: false,
+    },
+    {
+      description: "should be true for datetime day before",
+      input: {
+        isRecurring: false,
+        date: "2023-12-31",
+        datetime: "2023-12-31T12:00:00",
+      },
+      expected: true,
+    },
+    {
+      description: "should be false for datetime any other previous day",
+      input: {
+        isRecurring: false,
+        date: "2023-12-28",
+        datetime: "2023-12-28T12:00:00",
+      },
+      expected: false,
+    },
+    {
+      description: "should be false for datetime same day",
+      input: {
+        isRecurring: false,
+        date: "2024-01-01",
+        datetime: "2024-01-01T12:00:00",
+      },
+      expected: false,
+    },
+    {
+      description: "should be false for datetime any other future day",
+      input: {
+        isRecurring: false,
+        date: "2024-01-02",
+        datetime: "2024-01-02T12:00:00",
+      },
+      expected: false,
+    },
+  ];
+
+  for (const tc of testcases) {
+    it(tc.description, () => {
+      const info = new DueDate(tc.input);
+      const isTomorrow = info.isYesterday();
+      expect(isTomorrow).toEqual(tc.expected);
+    });
+  }
+});
+
+describe("isInLastWeek", () => {
+  const testcases: TestCase[] = [
+    {
+      description: "should be true for day in last week",
+      input: {
+        isRecurring: false,
+        date: "2023-12-28",
+      },
+      expected: true,
+    },
+    {
+      description: "should be false for any other previous day",
+      input: {
+        isRecurring: false,
+        date: "2023-12-24",
+      },
+      expected: false,
+    },
+    {
+      description: "should be false for same day",
+      input: {
+        isRecurring: false,
+        date: "2024-01-01",
+      },
+      expected: false,
+    },
+    {
+      description: "should be false for any other future day",
+      input: {
+        isRecurring: false,
+        date: "2024-01-02",
+      },
+      expected: false,
+    },
+    {
+      description: "should be true for datetime day in last week",
+      input: {
+        isRecurring: false,
+        date: "2023-12-28",
+        datetime: "2023-12-28T12:00:00",
+      },
+      expected: true,
+    },
+    {
+      description: "should be false for datetime any other previous day",
+      input: {
+        isRecurring: false,
+        date: "2023-12-24",
+        datetime: "2023-12-24T12:00:00",
+      },
+      expected: false,
+    },
+    {
+      description: "should be false for datetime same day",
+      input: {
+        isRecurring: false,
+        date: "2024-01-01",
+        datetime: "2024-01-01T12:00:00",
+      },
+      expected: false,
+    },
+    {
+      description: "should be false for any datetime other future day",
+      input: {
+        isRecurring: false,
+        date: "2024-01-02",
+        datetime: "2024-01-02T12:00:00",
+      },
+      expected: false,
+    },
+  ];
+
+  for (const tc of testcases) {
+    it(tc.description, () => {
+      const info = new DueDate(tc.input);
+      const isTomorrow = info.isInLastWeek();
+      expect(isTomorrow).toEqual(tc.expected);
+    });
+  }
+});
+
+describe("isInNextWeek", () => {
+  const testcases: TestCase[] = [
+    {
+      description: "should be false for any previous day",
+      input: {
+        isRecurring: false,
+        date: "2023-12-24",
+      },
+      expected: false,
+    },
+    {
+      description: "should be false for same day",
+      input: {
+        isRecurring: false,
+        date: "2024-01-01",
+      },
+      expected: false,
+    },
+    {
+      description: "should be true for future day within a week",
+      input: {
+        isRecurring: false,
+        date: "2024-01-02",
+      },
+      expected: true,
+    },
+    {
+      description: "should be false for any other future day",
+      input: {
+        isRecurring: false,
+        date: "2024-01-09",
+      },
+      expected: false,
+    },
+    {
+      description: "should be false for datetime any previous day",
+      input: {
+        isRecurring: false,
+        date: "2023-12-24",
+        datetime: "2023-12-24T12:00:00",
+      },
+      expected: false,
+    },
+    {
+      description: "should be false for dateimt same day",
+      input: {
+        isRecurring: false,
+        date: "2024-01-01",
+        datetime: "2024-01-01T12:00:00",
+      },
+      expected: false,
+    },
+    {
+      description: "should be true for datetime future day within a week",
+      input: {
+        isRecurring: false,
+        date: "2024-01-02",
+        datetime: "2024-01-02T12:00:00",
+      },
+      expected: true,
+    },
+    {
+      description: "should be false for datetime any other future day",
+      input: {
+        isRecurring: false,
+        date: "2024-01-09",
+        datetime: "2024-01-09T12:00:00",
+      },
+      expected: false,
+    },
+  ];
+
+  for (const tc of testcases) {
+    it(tc.description, () => {
+      const info = new DueDate(tc.input);
+      const isTomorrow = info.isInNextWeek();
+      expect(isTomorrow).toEqual(tc.expected);
+    });
+  }
+});
+
+describe("isCurrentYear", () => {
+  const testcases: TestCase[] = [
+    {
+      description: "should be true for current year",
+      input: {
+        isRecurring: false,
+        date: "2024-06-01",
+      },
+      expected: true,
+    },
+    {
+      description: "should be false for last year",
+      input: {
+        isRecurring: false,
+        date: "2023-06-01",
+      },
+      expected: false,
+    },
+    {
+      description: "should be false for next year",
+      input: {
+        isRecurring: false,
+        date: "2025-06-01",
+      },
+      expected: false,
+    },
+    {
+      description: "should be true for datetime current year",
+      input: {
+        isRecurring: false,
+        date: "2024-06-01",
+        datetime: "2024-06-01T12:00:00",
+      },
+      expected: true,
+    },
+    {
+      description: "should be false for datetime last year",
+      input: {
+        isRecurring: false,
+        date: "2023-06-01",
+        datetime: "2023-06-01T12:00:00",
+      },
+      expected: false,
+    },
+    {
+      description: "should be false for datetime next year",
+      input: {
+        isRecurring: false,
+        date: "2025-06-01",
+        datetime: "2025-06-01T12:00:00",
+      },
+      expected: false,
+    },
+  ];
+
+  for (const tc of testcases) {
+    it(tc.description, () => {
+      const info = new DueDate(tc.input);
+      const isTomorrow = info.isCurrentYear();
+      expect(isTomorrow).toEqual(tc.expected);
+    });
+  }
+});
