e04d68aea7eceba83aa118067e6f1a1452d152ef
Jamie Brynes <jamiebrynes7@gmail.com>
2025-07-12 13:02:21 +0000

    nix flake: update to 25.05 nixpkgs

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
   };
